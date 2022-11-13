4b1585f7ea0a722f00c8821bfb05b3ca8c58b151
Jasper Leung <jasper.leung123@gmail.com>
2022-11-12 19:51:29 -0500

    Added Start Screen, Made header refresh page

--- src/App.js
+++ src/App.js
@@ -9,23 +9,22 @@ import Staff from "./components/Staff";
 import 'bootstrap/dist/css/bootstrap.css'
 
 import StaffLogin from "./components/StaffLogin";
+import StartScreen from "./components/StartScreen";
 
 const App = () => {
   const [currentPageTab, setCurrentPageTab] = useState(
-    constants.PAGE_TABS.MENU
+    constants.PAGE_TABS.START_SCREEN
   );
 
   return (
     <div className="ros">
-      <Header />
-      {flags.isSignedIn ? (
-        <Navigator
-          tabs={constants.PAGE_TABS_CONFIG(setCurrentPageTab)}
-          activeTab={currentPageTab}
-          activeTabStyle="highlight-tab"
-          height="small-tab-height"
-        />
-      ) : null}
+      <Header setCurrentPageTab={setCurrentPageTab} />
+      <Navigator
+        tabs={constants.PAGE_TABS_CONFIG(setCurrentPageTab)}
+        activeTab={currentPageTab}
+        activeTabStyle="highlight-tab"
+        height="small-tab-height"
+      />
       {currentPageTab == constants.PAGE_TABS.MENU ? <Menu /> : ""}
       {currentPageTab == constants.PAGE_TABS.CART ? <Cart /> : ""}
       {currentPageTab == constants.PAGE_TABS.MENU ? (
@@ -50,6 +49,9 @@ const App = () => {
         : ""}
       {currentPageTab == constants.PAGE_TABS.CHANGE_MENU
         ? "Put change menu page component here"
+        : ""}
+      {currentPageTab == constants.PAGE_TABS.START_SCREEN
+        ? <StartScreen setCurrentPageTab={setCurrentPageTab} />
         : ""}
     </div>
   );
