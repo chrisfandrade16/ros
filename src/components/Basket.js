be2c4de94ebd4f6cf1634e1d871329f1d8bfe098
Muneeb Arshad <>
2022-11-17 18:59:44 -0500

    fix cart page

--- src/components/Basket.js
+++ src/components/Basket.js
@@ -1,138 +1,227 @@
 import React, { useState, useEffect } from "react";
 import "../styles/Cart.scss";
 import { LazyLoadImage } from "react-lazy-load-image-component";
+import emptyCart from "images/emptyCart.png";
 import {
-  ListGroup,
-  DropdownButton,
-  Dropdown,
-  InputGroup,
-  Form,
-} from "react-bootstrap";
+  List, ListItem, Select, Modal, ModalOverlay, ModalContent, ModalHeader, ModalFooter, ModalBody, ModalCloseButton, FormControl, FormLabel, Input,   extendTheme, InputRightAddon, InputGroup
+
+} from '@chakra-ui/react';
+
+import * as chakra from '@chakra-ui/react' ;
 import Button from "components/Button";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faTrash } from "@fortawesome/free-solid-svg-icons";
 
+
 export default function Basket(props) {
-  const { cartItems, onAdd, onRemove } = props;
+  const { cartItems, onAdd, onRemove, onRemoveAll} = props;
   const totalCost = cartItems.reduce((a, c) => a + c.cost * c.count, 0);
   const taxPrice = totalCost * 0.13;
-  const [tipAmount, setTipAmount] = useState(0.0);
+  // const [tipAmount, setTipAmount] = useState(0.0);
+  const [tipPercentage,setTipPercentage] = useState(0)
+  const tipAmount = totalCost * tipPercentage/100;
   const cartTotal = totalCost + taxPrice + tipAmount;
+  const [isOpen,setOpen] = useState(false);
+  const [paymentOptionSelected,setPaymentOption] = useState("");
+  const [paymentAlert,showPaymentAlert] = useState("none");
 
-  const handleChange = (event) => {
-    console.log(event.target.value);
+  const onChangePaymentOption = (event) => {
+    setPaymentOption(event.target.value)
   };
 
   const onChangeTipAmount = (event) => {
-    if (event.target.value != "0") {
-      const tipAmount = totalCost * event.target.value;
-      setTipAmount(tipAmount);
-    } else {
-      setTipAmount(0);
+      setTipPercentage(event.target.value)
+  };
+
+  const submitOrder = (event) => {
+    if(paymentOptionSelected == ""){
+      showPaymentAlert("")
+    }
+    else{
+      showPaymentAlert("none")
+      setOpen(true)
     }
   };
 
+  const cancel = (event) =>{
+    setOpen(false)
+  }
+
+  const removeAllItems = (itemName) =>{
+    onRemoveAll(itemName);
+    
+  }
+
+
+  const theme = extendTheme({
+    components: {
+      Modal: {
+        baseStyle: (props) => ({
+          dialog: {
+            bg: "#282935"
+          }
+        })
+      }
+    }
+  });
+
   return (
+
+
     <div class="basket">
-      {cartItems.map((item) => (
-        <div className="cartItem">
-          <div class="trashIcon">
-            <FontAwesomeIcon icon={faTrash} />
-          </div>
+      <div class="basketItems">
+        {cartItems.map((item) => (
+            <div className="cartItem">
+              <div class="trashIcon">
+                <FontAwesomeIcon icon={faTrash} onClick={() => removeAllItems(item.name)}/>
+              </div>
 
-          <LazyLoadImage
-            src={item.img}
-            width={175}
-            height={150}
-            alt={item.name}
-            style={{ alignSelf: "center" }}
-            effect="blur"
-          />
-          <div className="cartItemDesc" style={{ userSelect: "none" }}>
-            <div>{item.name}</div>
-            {item.size && <div>Size: {item.size}</div>}
-          </div>
+              <LazyLoadImage
+                src={item.img}
+                width={175}
+                height={150}
+                alt={item.name}
+                style={{ alignSelf: "center" }}
+                effect="blur"
+              />
+              <div className="cartItemDesc" style={{ userSelect: "none" }}>
+                <div>{item.name}</div>
+                {item.size && <div>Size: {item.size}</div>}
+              </div>
 
-          <div class="basketMiddle">
-            <div class="cartItemCost">${item.cost}</div>
-            <div class="cartItemCount">
-              <Button
-                color="red"
-                content="-"
-                onClick={() => onRemove(item.name)}
-              ></Button>
-              <div class="itemCount">{item.count}</div>
-              <Button
-                color="green"
-                content="+"
-                onClick={() => onAdd(item.name)}
-              ></Button>
+              <div class="basketMiddle">
+                <div class="cartItemCost">${item.cost}</div>
+                <div class="cartItemCount">
+                  <Button
+                    width="8"
+                    content="-"
+                    onClick={() => onRemove(item.name)}
+                  ></Button>
+                  <div class="itemCount">{item.count}</div>
+                  <Button
+                    width="8"
+                    color="green"
+                    content="+"
+                    onClick={() => onAdd(item.name)}
+                  ></Button>
+                </div>
+              </div>
+
+              <div class="basketLeft">
+                <div>${(item.cost * item.count).toFixed(2)}</div>
+              </div>
             </div>
-          </div>
+          ))}
+        {cartItems.length === 0 && <div><img class="cartEmpty" src={emptyCart}></img></div>}
+      </div>
 
-          <div class="basketLeft">
-            <div>${(item.cost * item.count).toFixed(2)}</div>
-          </div>
-        </div>
-      ))}
-      {cartItems.length === 0 && <div>Cart is empty</div>}
       {cartItems.length !== 0 && (
         <div class="basketFooter">
-          <div class="backButton">
-            <Button color="red" content="Back to Menu"></Button>
-          </div>
-
-          <div class="footerMid">
-            <div class="footerOptions">
-              <div class="paymentOptions">
-                <Form.Select
-                  onChange={handleChange}
-                  aria-label="Default select example"
-                >
-                  <option>Payment Option</option>
-                  <option value="Debit">Debit</option>
-                  <option value="1">Credit</option>
-                  <option value="2">In person</option>
-                </Form.Select>
-              </div>
-              <div class="submitButton">
-                <Button color="green" content="Submit Order"></Button>
-              </div>
-            </div>
-          </div>
-
           <div class="orderSummary">
-            <ListGroup>
-              <ListGroup.Item>
+            <List spacing={3}>
+              <ListItem>
                 <div class="orderSummaryRow">
                   <div class="orderSummaryTitle">Cost:</div>
                   <div class="orderSummaryValue">${totalCost.toFixed(2)}</div>
                 </div>
-                 
-                </ListGroup.Item>
-              <ListGroup.Item>
+              </ListItem>
                 <div class="orderSummaryRow">
                   <div class="orderSummaryTitle">Tax:</div>
                   <div class="orderSummaryValue">${taxPrice.toFixed(2)}</div>
                 </div>
-                </ListGroup.Item>
-              <ListGroup.Item id="tipRow">
-                Tip: ${tipAmount.toFixed(2)}
-                <div class="tipOptions">
-                  <Form.Select size="sm" onChange={onChangeTipAmount}>
-                    <option value="0">Tip Amount</option>
-                    <option value="0.05">5%</option>
-                    <option value="0.1">10%</option>
-                    <option value="0.15">15%</option>
-                    <option value="0.20">20%</option>
-                  </Form.Select>
+              <ListItem>
+                <div class="orderSummaryRow">
+                  <div class="orderSummaryTitle">Tip:</div>
+                  <div class="orderSummaryValue">${tipAmount.toFixed(2)}</div>
+                  <div class="tipOptions">
+                    <InputGroup size='xs' onChange={onChangeTipAmount}>
+                      <Input style={{width:"55px"}} placeholder='Tip' />
+                      <InputRightAddon style={{color:"black"}} children='%' />
+                    </InputGroup>
+                  </div>
                 </div>
-              </ListGroup.Item>
-              <ListGroup.Item>Total: ${cartTotal.toFixed(2)}</ListGroup.Item>
-            </ListGroup>
+              </ListItem>
+              <ListItem>
+                <div class="orderSummaryRow">
+                  <div class="orderSummaryTitle">Total:</div>
+                  <div class="orderSummaryValue">${cartTotal.toFixed(2)}</div>
+                </div>
+              </ListItem>
+            </List>
+
+            <div class="footerOptions"> 
+              <div class="paymentOptions">
+                  <Select size='sm' onChange={onChangePaymentOption}>
+                    <option value="">Payment Option</option>
+                    <option  value="Debit">Debit</option>
+                    <option value="Credit">Credit</option>
+                    <option value="Inperson"> In person</option>
+                  </Select>
+                </div>
+                <chakra.Button colorScheme='blue' size='sm' onClick={() => {submitOrder()}}> 
+                  Submit
+                </chakra.Button>
+            </div>
+
+            <div style={{display:paymentAlert}} class="paymentAlert"> Please select payment option</div>
+
           </div>
         </div>
       )}
+
+    <Modal theme={theme} closeOnOverlayClick={false} isOpen={isOpen} onClose={cancel}>
+          <ModalOverlay />
+          <ModalContent>
+            <ModalHeader>{["Debit","Credit"].includes(paymentOptionSelected) &&  <div>Enter payment information</div>}</ModalHeader>
+            <ModalCloseButton />
+            <ModalBody pb={6}>
+              {["Debit","Credit"].includes(paymentOptionSelected) && <div class="modalBody">
+
+                <div class="cardDetailsRow">
+                  <FormControl isRequired>
+                    <FormLabel>Cardholder Name</FormLabel>
+                    <Input placeholder='Cardholder Name' />
+                  </FormControl>
+                  <FormControl style={{paddingLeft:"10px"}} isRequired>
+                    <FormLabel>Expiry</FormLabel>
+                    <div style={{display:"flex"}}>
+                      <div style={{paddingRight:"10px"}}><Input placeholder='Month' /></div>
+                      <div><Input placeholder='Year' /></div>
+                    </div>
+                  </FormControl>
+                </div>
+
+                <div class="cardDetailsRow">
+                  <FormControl isRequired>
+                    <FormLabel>Cardholder Name</FormLabel>
+                    <Input placeholder='Card Number' />
+                  </FormControl>
+                  <FormControl style={{paddingLeft:"10px"}} isRequired>
+                    <FormLabel>CVC</FormLabel>
+                    <div>
+                      <div><Input placeholder='CVC' /></div>
+                    </div>
+                  </FormControl>
+                </div>
+              </div>}
+            </ModalBody>
+
+            <ModalBody pb={6}>
+              {paymentOptionSelected == "Inperson" && <div style={{paddingLeft:"30px"}} class="modalBody">
+
+                Hang Tight! Server will be with you shortly.
+
+                
+              </div>}
+            </ModalBody>
+
+            <ModalFooter>
+              {["Debit","Credit"].includes(paymentOptionSelected) && <chakra.Button colorScheme='blue' mr={3}>Complete Order</chakra.Button>}
+              <chakra.Button colorScheme='gray' onClick={cancel}><a style={{color:"black"}}>Cancel</a></chakra.Button>
+            </ModalFooter>
+          </ModalContent>
+      </Modal>
+
     </div>
   );
 }
--- src/components/Cart.js
+++ src/components/Cart.js
@@ -1,9 +1,24 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import Basket from "./Basket";
+import {
+    AlertDialog,
+    AlertDialogBody,
+    AlertDialogFooter,
+    AlertDialogHeader,
+    AlertDialogContent,
+    AlertDialogOverlay,
+    useDisclosure
+  } from '@chakra-ui/react'
+  import * as chakra from '@chakra-ui/react' ;
+
 
 export default function Cart({data}) {
    
    const [cartItems, setCartItems] = useState(data.getCartItems(["Large Pepperoni Pizza", "Small Pepperoni Pizza", "Cheese Burger"]))
+   const [showConfirmation,setShowConfirmation] = useState(false)
+   const [itemToBeDeleted,setItemToBeDeleted] = useState("")
+   const {onClose } = useDisclosure()
+   const cancelRef = React.useRef()
 
    const onAdd = (itemName) => {
         const exists = cartItems.find((x) => x.name === itemName);
@@ -18,23 +33,69 @@ export default function Cart({data}) {
 
 
    const onRemove = (itemName) => {
-    const exists = cartItems.find((x) => x.name === itemName);
-    if(exists.count === 1){
-        setCartItems(cartItems.filter((x) => x.name !== itemName))
+        const exists = cartItems.find((x) => x.name === itemName);
+        if(exists.count === 1){
+            setShowConfirmation(true)
+            setItemToBeDeleted(itemName)
+        }
+        else{
+            setCartItems(
+                cartItems.map((x) =>
+                x.name === itemName ? {...exists, count: parseInt(exists.count) - 1} : x
+                )
+            );
+        }
     }
-    else{
-        setCartItems(
-            cartItems.map((x) =>
-            x.name === itemName ? {...exists, count: parseInt(exists.count) - 1} : x
-            )
-        );
+
+    const onRemoveAll = (itemName) => {
+        const exists = cartItems.find((x) => x.name === itemName);
+        if(exists){
+            setShowConfirmation(true)
+            setItemToBeDeleted(itemName)
+        }
     }
-}
+
+    const deleteLastOrder = (event) => {
+        setCartItems(cartItems.filter((x) => x.name !== itemToBeDeleted))
+        setShowConfirmation(false)
+    }
+
+    const cancel = (event)=>{
+        setShowConfirmation(false)
+
+    }
+
+
 
   return (
     <div id="cart-container">
-        <Basket cartItems={cartItems} onAdd={onAdd} onRemove = {onRemove}></Basket>
-      
+        <Basket cartItems={cartItems} onAdd={onAdd} onRemove = {onRemove} onRemoveAll={onRemoveAll}></Basket>
+        <AlertDialog
+            isOpen={showConfirmation}
+            leastDestructiveRef={cancelRef}
+            onClose={onClose}
+        >
+            <AlertDialogOverlay>
+            <AlertDialogContent>
+                <AlertDialogHeader fontSize='lg' fontWeight='bold'>
+                Delete Item
+                </AlertDialogHeader>
+
+                <AlertDialogBody>
+                Are you sure? You can't undo this action afterwards.
+                </AlertDialogBody>
+
+                <AlertDialogFooter>
+                <chakra.Button style={{color:"black"}} ref={cancelRef} onClick={cancel}>
+                    Cancel
+                </chakra.Button>
+                <chakra.Button colorScheme='red' onClick={deleteLastOrder} ml={3}>
+                    Delete
+                </chakra.Button>
+                </AlertDialogFooter>
+            </AlertDialogContent>
+            </AlertDialogOverlay>
+        </AlertDialog>
     </div>
   );
 }
