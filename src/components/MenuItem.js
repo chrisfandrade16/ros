16514c07cd76c1d49d92b4d0a2b3e218bebf7ccb
lim211 <lim211@mcmaster.ca>
2022-11-17 03:31:51 -0500

    updates

--- src/components/MenuItem.js
+++ src/components/MenuItem.js
@@ -16,7 +16,7 @@ export default function MenuItem({ info, setTotalCost, setTotalItems, clear }) {
         effect="blur"
       />
       <div className="item-desc" style={{ userSelect: "none" }}>
-        <div>{info.name}</div>
+        <div>Name: {info.name}</div>
         <div>Ingredients: {info.ingredients.join(", ")}</div>
         {info.size && <div>Size: {info.size}</div>}
       </div>
